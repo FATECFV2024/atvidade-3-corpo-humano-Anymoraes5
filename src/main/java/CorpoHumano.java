73f202ada2bb7bdffe5c38d53883aa1ed35f8eca
Anymoraes5 <moraesany44@gmail.com>
2024-09-20 12:20:41 -0300

    [feat] corpo humano add classe corpo humano

--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,5 +1,52 @@
 package main.java;
 
 public class CorpoHumano {
+    private int massa;
+    private double volume;
+    private int densidade;
+    private float altura;
+
+    public CorpoHumano(int massa, double volume, int densidade, float altura) {
+        this.massa = massa;
+        this.volume = volume;
+        this.densidade = densidade;
+        this.altura = altura;
+    }
+
+    public int getMassa() {
+        return massa;
+    }
+
+    public void setMassa(int massa) {
+        this.massa = massa;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public int getDensidade() {
+        return densidade;
+    }
+
+    public void setDensidade(int densidade) {
+        this.densidade = densidade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public double calcularIMC(){
+        return massa/(altura*altura);
+    }
 
 }
